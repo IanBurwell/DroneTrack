bac7f12d6b5185a47a3ad183d8e2404dc97ce895
Ian <spyguyian@gmail.com>
2017-11-27 12:13:56 -0500

    Used DroneKit's example code to move position instead of whatever i did before

--- Tests/DroneSpin.py
+++ Tests/DroneSpin.py
@@ -1,12 +1,9 @@
-from dronekit import connect, VehicleMode, LocationGlobalRelative
-import time
-import sys
+from DroneHelpers import *
+import argparse
+import buttonstart
+
 
 # Set up option parsing to get connection string
-import argparse
-
-from pymavlink import mavutil
-
 parser = argparse.ArgumentParser(
     description='Print out vehicle state information. Connects to SITL on local PC by default.')
 parser.add_argument('--connect',
@@ -33,15 +30,15 @@ vehicle.wait_ready('autopilot_version')
 pause = False
 
 print 'vehicle ready'
-import buttonstart 
 buttonstart.wait_go_button()
+
 
 # safety precaustions
 def ch6_change(self, attr_name, channels):
     value = channels['6']
-    #print 'channel changed ',value,' ',channels
+    # print 'channel changed ',value,' ',channels
     if value > 1750:
-	print 'Aborting and landing'
+        print 'Aborting and landing'
         vehicle.mode = VehicleMode('LAND')
         print "Close vehicle object"
         vehicle.close()
@@ -50,7 +47,7 @@ def ch6_change(self, attr_name, channels):
             sitl.stop()
         sys.exit(1)
     elif value > 1100:
-	print 'paused'
+        print 'paused'
         pause = True
     else:
         pause = False
@@ -58,69 +55,16 @@ def ch6_change(self, attr_name, channels):
 
 vehicle.add_attribute_listener("channels", ch6_change)
 
-
-def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude (meters).
-    """
-
-    print "Basic pre-arm checks"
-    # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print " Waiting for vehicle to initialise..."
-        time.sleep(1)
-
-    print "Arming motors"
-    # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True
-
-    # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
-        print " Waiting for arming..."
-        time.sleep(1)
-
-    print "Taking off!"
-    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
-
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.simple_takeoff will execute immediately).
-    while True:
-        print " Altitude: ", vehicle.location.global_relative_frame.alt
-        # Break and return from function just below target altitude.
-        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
-            print "Reached target altitude"
-            break
-        time.sleep(1)
-
-
-def condition_yaw(heading, relative=False):
-    if relative:
-        is_relative=1 #yaw relative to direction of travel
-    else:
-        is_relative=0 #yaw is an absolute angle
-    # create the CONDITION_YAW command using command_long_encode()
-    msg = vehicle.message_factory.command_long_encode(
-        0, 0,    # target system, target component
-        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
-        0, #confirmation
-        heading,    # param 1, yaw in degrees
-        0,          # param 2, yaw speed deg/s
-        1,          # param 3, direction -1 ccw, 1 cw
-        is_relative, # param 4, relative offset 1, absolute angle 0
-        0, 0, 0)    # param 5 ~ 7 not used
-    # send command to vehicle
-    vehicle.send_mavlink(msg)
-
-arm_and_takeoff(4)
+arm_and_takeoff(vehicle, 4)
 time.sleep(4)
 
-print 'going to current location'
-a_location = LocationGlobalRelative(0, 0, 0)
-vehicle.simple_goto(a_location)
+print "Set groundspeed to 2m/s (max)."
+vehicle.groundspeed = 2
+print "Position South 0 East 0"
+goto(vehicle, 0, 0, goto_position_target_global_int)
 
 print 'Turning'
-condition_yaw(90, True)
+condition_yaw(vehicle, 540, True)
 time.sleep(4)
 
 print 'landing'
